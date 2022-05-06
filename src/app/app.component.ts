b65af83c54504dc29b471775f26aec2c29e85ea4
Dylan Ellington <dylanellington07@gmail.com>
2022-05-05 19:51:34 -0500

    implemented api information to code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { PokemonService } from 'src/services/pokemon.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PokeDEX';
+  pokemon: any = {}
+  constructor(
+    private pokemonService: PokemonService
+    ) {}  
+  ngOnInit(): void {
+    this.pokemonService.getPokemon().subscribe((pokemon) => { 
+      this.pokemon = pokemon
+    } );
+  }
 }
+// @Component({
+//   selector: 'pokemon-card',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+
+// export class PokemonCard implements OnInit {
+//   pokemon: string = "ditto"
+//   constructor(
+//     private pokemonService: PokemonService
+//     ) {}  
+//   ngOnInit(): void {
+//     this.pokemonService.getPokemon().subscribe((pokemon) => { 
+//       this.pokemon = pokemon
+//     } );
+//   }
+// }
