ba82f9aa44ce9aa0cb0f136dc7474c1af0fde7fd
Dylan Ellington <dylanellington07@gmail.com>
2022-06-14 14:23:42 -0500

    close up commit

--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -9,20 +9,12 @@ export class PokemonService {
 
   constructor( private http: HttpClient ) { }
   private baseUrl: string = "/api"
-  getPokemon(): Observable<SimplePokemonResponse> {
-    return this.http.get<SimplePokemonResponse>(`${this.baseUrl}/pokemon?limit=10&offset=0/`)
+  getPokemon(): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/pokemon?limit=10&offset=0/`)
   }
   
-  /*code below is an attempted function to add images from api*/
-  getArtwork(url: string): Observable<any> {
-    let endpoint = url.split("v2")[1]
-    return this.http.get<any>(`${this.baseUrl}${endpoint}`)
-  }
-}
-export class SimplePokemonResponse {
-  results!: SimplePokemon[]
-}
-export class SimplePokemon {
-  name!: string
-  url!: string
+  // /*code below is an attempted function to add images from api*/
+  // getArtwork(): Observable<any> {
+  //   return this.http.get<any>(`${this.baseUrl}/pokemon/psyduck/`)
+  // }
 }
